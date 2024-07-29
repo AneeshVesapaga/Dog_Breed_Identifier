48c777d86584658b5e5ad481dba99057c8b24532
AneeshVesapaga <164468484+AneeshVesapaga@users.noreply.github.com>
2024-07-29 16:38:00 +0530

    Update breed.py

--- breed.py
+++ breed.py
@@ -2,30 +2,22 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import joblib
-
-
-# Attempt to import cv2 and handle potential import errors
-
+import cv2
 
 # Display title
-st.title('Dog Breed Classification App')
+image_path = 'ino_img.jpg'  # Replace with your actual PNG image file path
 
-# Display an example image
-image_path = 'ino_img.jpg'  # Replace with your actual image file path
-st.image(image_path, caption='Example Image')
+# Display the PNG image
+st.image(image_path, caption='ino_img.jpg')
 
 # Load the pre-trained model
 model_path = "lr.pkl"
-try:
-    model = joblib.load(model_path)
-    st.success('Model loaded successfully.')
-except Exception as e:
-    st.error(f'Error loading model: {e}')
+model = joblib.load(model_path)
 
 # Function to preprocess the image
 def preprocess_image(image):
     try:
-        # Resize the image to (200, 200) as per your model's input requirement
+        # Resize the image to (20, 20) as per your model's input requirement
         resized_image = image.resize((200, 200))
         # Convert to numpy array and flatten
         img_array = np.array(resized_image)
@@ -34,10 +26,10 @@ def preprocess_image(image):
             img_array = cv2.cvtColor(img_array, cv2.COLOR_RGB2GRAY)
         
         flattened_img = img_array.flatten()
-        # Ensure the flattened image has exactly 40000 features
+        # Ensure the flattened image has exactly 400 features
         if flattened_img.shape[0] != 40000:
             raise ValueError(f"Expected 40000 features, but got {flattened_img.shape[0]} features.")
-        # Reshape to (1, 40000) to match model's input shape
+        # Reshape to (1, 400) to match model's input shape
         processed_image = flattened_img.reshape(1, -1)
         return processed_image
     except Exception as e:
@@ -46,6 +38,8 @@ def preprocess_image(image):
 
 # Streamlit app
 def main():
+    st.title('Dog Breed Classification App')
+
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])  # Adjust type as per your image types
 
     if uploaded_file is not None:
@@ -71,5 +65,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
